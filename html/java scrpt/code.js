134c7e86391a786b85fcd5c296ab218dd8569561
zeeshan <zkthedev@gmail.com>
2020-03-26 20:17:00 +0500

    ok so these are many of my old creations
    they are very cool

---
+++ html/java scrpt/code.js
@@ -0,0 +1,31 @@
+
+/*var age = prompt("Tell me your age");
+document.write("your age is",age);
+
+
+document.write("5 + 9 = ", 5+9,"<br>");
+document.write("5 - 9 = ", 5-9,"<br>");
+document.write("5 * 9 = ", 5*9,"<br>");
+document.write("5 / 9 = ", 5/9,"<br>");
+document.write("5 % 9 = ", 5%9,"<br>");
+
+
+
+
+// my age teller
+var age = prompt("Tell me your age");
+
+var name = prompt("Tell me your name");
+document.write("Your age is ", age,"<br>");
+document.write("Your name is ", name,"<br>");
+
+document.write("2^9 = ", Math.pow(2,9),"<br>" );
+document.write("abs(-34) = ", Math.abs(-34),"<br>");
+document.write("sqrt(36) = ", Math.sqrt(36),"<br>");
+
+ */
+var foodcost = prompt("What is the total amount");
+var no = prompt("No of people");
+document.write("The total cost  was ", foodcost, "<br>");
+document.write("Each one of you has to pay ", (foodcost/no).toFixed(2),"<br>");
+
---
+++ html/win somthing/script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "honda";
+  }
+  function first() {
+    document.getElementById("first").innerHTML = "buggati";
+  }
+  function r() {
+    document.getElementById("r").innerHTML = "lamborgini";
+  }
+  function q() {
+    document.getElementById("q").innerHTML = "nothing";
+  }
